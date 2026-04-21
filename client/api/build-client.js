1f1904bafbd7911debd6a48c5b3e82db4d607a8b
Emmsfay <emmychukwudi215@gmail.com>
2026-04-21 13:37:42 +0100

    Add Azure IaC, pipeline, K8s prod manifests and README

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -2,17 +2,17 @@ import axios from 'axios'
 
 export default ({ req }) => {
   if (typeof window === 'undefined') {
-    // We are on the server
-
     return axios.create({
-      baseURL: process.env.NODE_ENV === 'production' ? 'https://www.aurapan.com' : 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
-      headers: req.headers,
-      withCredentials: true
+      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      headers: {
+        ...req.headers,
+        host: 'aurapan.40.127.156.53.nip.io',
+      },
+      withCredentials: true,
     })
   } else {
-    // We must be on the browser
     return axios.create({
-      baseUrl: '/'
+      baseURL: '/',
     })
   }
 }
